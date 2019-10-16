2a9ea9b5bd0bfc4d1e12f870b6ebf354e5806d16
Caspian Baska <caspianbaska@gmail.com>
2019-10-16 16:04:27 +1100

    feat(config): update logging

--- src/config.cr
+++ src/config.cr
@@ -2,15 +2,12 @@
 require "action-controller"
 PROD = ENV["SG_ENV"]? == "production"
 
-# Allows request IDs to be configured for logging
-# You can extend this with additional properties
+# Logging configuration
 ActionController::Logger.add_tag request_id
-filter_params = ["bearer_token", "password"]
 logger = ActionController::Base.settings.logger
 logger.level = PROD ? Logger::INFO : Logger::DEBUG
 
-# For some reason I could not convince Crystal this was not a module without
-# putting it here
+# Required to convince Crystal this file is not a module
 abstract class ACAEngine::Driver; end
 
 class ACAEngine::Driver::Protocol; end
@@ -27,35 +24,17 @@ require "action-controller/server"
 
 # Configure Service discovery
 HoundDog.configure do |settings|
-  settings.logger = ActionController::Base.settings.logger
+  settings.logger = logger
   settings.etcd_host = ENV["ETCD_HOST"]? || "localhost"
   settings.etcd_port = (ENV["ETCD_PORT"]? || 2379).to_i
 end
 
+# Filter out sensitive params that shouldn't be logged
+filter_params = ["password", "bearer_token"]
+
 # Add handlers that should run before your application
 ActionController::Server.before(
   HTTP::ErrorHandler.new(!PROD),
-  ActionController::LogHandler.new(filter_params),
+  ActionController::LogHandler.new(PROD ? filter_params : nil),
   HTTP::CompressHandler.new
 )
-
-# Optional support for serving of static assests
-static_file_path = ENV["PUBLIC_WWW_PATH"]? || "./www"
-if File.directory?(static_file_path)
-  # Optionally add additional mime types
-  ::MIME.register(".yaml", "text/yaml")
-
-  # Check for files if no paths matched in your application
-  ActionController::Server.before(
-    ::HTTP::StaticFileHandler.new(static_file_path, directory_listing: false)
-  )
-end
-
-# Configure session cookies
-# NOTE:: Change these from defaults
-ActionController::Session.configure do |settings|
-  settings.key = ENV["COOKIE_SESSION_KEY"]? || "_spider_gazelle_"
-  settings.secret = ENV["COOKIE_SESSION_SECRET"]? || "4f74c0b358d5bab4000dd3c75465dc2c"
-  # HTTPS only:
-  settings.secure = PROD
-end
