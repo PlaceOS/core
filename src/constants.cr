43a4469df752b88c6c7d16d99db2a9b69ed53d0c
Stephen von Takach <steve@acaprojects.com>
2020-02-17 16:44:38 +1100

    fix(constants): improved version extraction

--- src/constants.cr
+++ src/constants.cr
@@ -1,5 +1,5 @@
 module ACAEngine::Core
   APP_NAME    = "engine-core"
   API_VERSION = "v1"
-  VERSION     = `shards version`
+  VERSION     = {{ `shards version "#{__DIR__}"`.chomp.stringify.downcase }}
 end
