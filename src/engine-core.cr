135fcc817a27ba5dc345482919e53982f8ad0789
Stephen von Takach <steve@acaprojects.com>
2020-02-29 17:18:08 +1100

    Revert "feat: use environment variables to expose host details"
    This reverts commit ae1c140cb60c994cdd87a3b75b8df49fe43f1c23.

--- src/engine-core.cr
+++ src/engine-core.cr
@@ -3,12 +3,7 @@ require "./engine-core/*"
 module ACAEngine::Core
   def self.start_managers
     resource_manager = ResourceManager.instance
-
-    # In k8s we can grab the Pod information from the environment
-    # https://kubernetes.io/docs/tasks/inject-data-application/environment-variable-expose-pod-information/#use-pod-fields-as-values-for-environment-variables
-    core_host = ENV["CORE_HOST"]? || "localhost"
-    core_port = (ENV["CORE_PORT"]? || "3000").to_i
-    module_manager = ModuleManager.instance(URI.new("http", core_host, core_port))
+    module_manager = ModuleManager.instance
 
     # Acquire resources on startup
     resource_manager.start do
