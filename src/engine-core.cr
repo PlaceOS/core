ae1c140cb60c994cdd87a3b75b8df49fe43f1c23
Stephen von Takach <steve@acaprojects.com>
2020-02-29 17:12:43 +1100

    feat: use environment variables to expose host details
    for use with etcd

--- src/engine-core.cr
+++ src/engine-core.cr
@@ -3,7 +3,12 @@ require "./engine-core/*"
 module ACAEngine::Core
   def self.start_managers
     resource_manager = ResourceManager.instance
-    module_manager = ModuleManager.instance
+
+    # In k8s we can grab the Pod information from the environment
+    # https://kubernetes.io/docs/tasks/inject-data-application/environment-variable-expose-pod-information/#use-pod-fields-as-values-for-environment-variables
+    core_host = ENV["CORE_HOST"]? || "localhost"
+    core_port = (ENV["CORE_PORT"]? || "3000").to_i
+    module_manager = ModuleManager.instance(URI.new("http", core_host, core_port))
 
     # Acquire resources on startup
     resource_manager.start do
