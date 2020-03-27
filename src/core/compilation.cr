0f7e3373bf91308c749c611ca40cc8e778d7115a
Caspian Baska <caspianbaska@gmail.com>
2020-03-28 01:39:29 +1100

    fix(resource:compilation): reload modules if the binary exists

--- src/core/compilation.cr
+++ src/core/compilation.cr
@@ -56,67 +56,69 @@ module PlaceOS
       repository = driver.repository.as(Model::Repository)
       repository_name = repository.folder_name.as(String)
 
-      update_commit = false
-      # If the commit is `head` then the driver must be recompiled.
-      if commit == "head"
+      if !driver.commit_changed? && Drivers::Helper.compiled?(file_name, commit)
+        logger.tag_info(
+          message: "commit unchanged and driver already compiled",
+          name: name,
+          file_name: file_name,
+          commit: commit,
+          repository_name: repository_name,
+        )
+
+        Compilation.reload_modules(driver, logger)
+        return {true, ""}
+      end
+
+      # If the commit is `head` then the driver must be recompiled at the latest version
+      if Compilation.pull?(commit)
         begin
           Cloning.clone_and_install(repository)
-          update_commit = ModuleManager.instance.discovery.own_node?(driver_id) || startup
         rescue e
           return {false, "failed to pull and install #{repository_name}: #{e.try &.message}"}
         end
-      elsif Drivers::Helper.compiled?(file_name, commit)
-        logger.tag_info("driver already compiled", name: name, repository_name: repository_name, commit: commit)
-        return {true, ""}
       end
 
       result = Drivers::Helper.compile_driver(file_name, repository_name)
       success = result[:exit_status] == 0
 
-      if success
-        logger.tag_info("compiled driver", name: name, repository_name: repository_name, output: result[:output])
-        module_manager = ModuleManager.instance
-
-        # Set when a module_manager found for stale driver
-        driver_path = nil
-
-        driver.modules.each do |mod|
-          module_id = mod.id.as(String)
-
-          # Remove the module running on the stale driver
-          driver_path = module_manager.path_for?(module_id)
-          module_manager.remove_module(mod)
-
-          if module_manager.started?
-            # Reload module on new driver binary
-            logger.tag_debug("reloading module after compilation", module_id: module_id, path: driver_path, driver_id: driver_id)
-            module_manager.load_module(mod)
-          end
-        end
-
-        # Remove the stale driver if there is one and there are no more modules running on it.
-        if driver_path && module_manager.proc_manager_by_driver?(driver_path)
-          remove_stale_driver(driver_path, driver_id, logger)
-        end
-
-        if update_commit
-          # Bump the commit on the driver post-compilation and module loading
-          update_driver_commit(driver: driver, commit: result[:version], startup: startup, logger: logger)
-        end
-      else
-        logger.tag_error("failed to compile driver", name: {name}, repository_name: repository_name, output: result[:output])
+      unless success
+        logger.tag_error("failed to compile driver: #{result[:output]}", repository_name: repository_name)
+        return {false, "failed to compile #{name} from #{repository_name}: #{result[:output]}"}
       end
 
-      {success, result[:output]}
+      logger.tag_info(
+        message: "compiled driver",
+        name: name,
+        repository_name: repository_name,
+        output: result[:output]
+      )
+
+      # (Re)load modules onto the newly compiled driver
+      stale_path = Compilation.reload_modules(driver, logger)
+
+      # Remove the stale driver if there was one
+      remove_stale_driver(
+        path: stale_path,
+        driver_id: driver_id,
+        logger: logger
+      )
+
+      # Bump the commit on the driver post-compilation and module loading
+      if Compilation.pull?(commit) && (startup || ModuleManager.instance.discovery.own_node?(driver_id))
+        update_driver_commit(driver: driver, commit: result[:version], startup: startup, logger: logger)
+      end
+
+      {success, ""}
     end
 
-    def self.remove_stale_driver(path : String, driver_id : String, logger)
+    # Remove the stale driver binary if there was one
+    #
+    def self.remove_stale_driver(path : String?, driver_id : String, logger)
+      return unless path
       logger.tag_info("removing stale driver binary", path: path, driver_id: driver_id)
-      begin
-        File.delete(path)
-      rescue
-        logger.tag_error("failed to remove stale driver binary", path: path, driver_id: driver_id)
-      end
+      File.delete(path) if File.exists?(path)
+    rescue
+      logger.tag_error("failed to remove stale driver binary", path: path, driver_id: driver_id)
     end
 
     def self.update_driver_commit(driver : Model::Driver, commit : String, startup : Bool, logger)
@@ -127,6 +129,46 @@ module PlaceOS
 
       driver.update_fields(commit: commit)
       logger.tag_info("updated commit on driver", name: driver.name, id: driver.id, commit: commit)
+    end
+
+    protected def self.pull?(commit : String?)
+      commit == "head"
+    end
+
+    # Returns the stale driver path
+    #
+    protected def self.reload_modules(driver : Model::Driver, logger)
+      module_manager = ModuleManager.instance
+
+      # Set when a module_manager found for stale driver
+      stale_path = driver.modules.reduce(nil) do |path, mod|
+        module_id = mod.id.as(String)
+
+        # Grab the stale driver path, if there is one
+        path = module_manager.path_for?(module_id) unless path
+
+        # Remove the module running on the stale driver
+        module_manager.remove_module(mod)
+
+        if module_manager.started?
+          # Reload module on new driver binary
+          logger.tag_debug(
+            message: "loading module after compilation",
+            module_id: module_id,
+            file_name: driver.file_name,
+            commit: driver.commit,
+            driver_id: driver.id,
+          )
+          module_manager.load_module(mod)
+        end
+
+        path
+      end
+
+      stale_path || driver.commit_was.try { |commit|
+        # Try to create a driver path from what the commit used to be
+        Drivers::Helper.driver_binary_path(driver.file_name.as(String), commit)
+      }
     end
 
     def start
