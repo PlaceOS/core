1e9e8d2d5b8a0a266bd689b25be9a395db169d82
Caspian Baska <caspianbaska@gmail.com>
2020-04-14 13:22:05 +1000

    test(controllers:command): spec `../drivers/:file_name/compiled`

--- spec/controllers/drivers_spec.cr
+++ spec/controllers/drivers_spec.cr
@@ -12,9 +12,8 @@ module PlaceOS::Core
         repo, _, _ = create_resources
 
         path = "#{namespace}?repository=#{repo.folder_name}"
-        io = IO::Memory.new
         ctx = context("GET", path, json_headers)
-        ctx.response.output = io
+        ctx.response.output = IO::Memory.new
         Api::Drivers.new(ctx, :index).index
 
         result = begin
@@ -25,7 +24,32 @@ module PlaceOS::Core
 
         ctx.response.status_code.should eq 200
 
-        result.should eq [SPEC_DRIVER]
+        result.should eq ["drivers/place/feature_test", "drivers/place/private_helper.cr"]
+      end
+    end
+
+    describe "drivers/:id/compiled" do
+      it "checks if a driver has been compiled" do
+        repo, driver, _ = create_resources
+        uri = URI.encode_www_form(SPEC_DRIVER)
+
+        params = HTTP::Params{
+          "repository" => repo.folder_name.as(String),
+          "commit"     => driver.commit.as(String),
+          "tag"        => driver.id.as(String),
+        }
+
+        path = File.join(namespace, uri, "/compiled?#{params}")
+        ctx = context("GET", path, json_headers)
+        ctx.route_params = {"id" => uri}
+        ctx.response.output = IO::Memory.new
+        Api::Drivers.new(ctx, :index).show
+
+        ctx.response.status_code.should eq 200
+
+        expected = PlaceOS::Drivers::Helper.commits(URI.decode(uri), repo.folder_name, 50)
+        result = Array(PlaceOS::Drivers::GitCommands::Commit).from_json(ctx.response.output.to_s)
+        result.should eq expected
       end
     end
 
@@ -34,11 +58,10 @@ module PlaceOS::Core
         repo, _, _ = create_resources
         uri = URI.encode_www_form(SPEC_DRIVER)
 
-        io = IO::Memory.new
         path = File.join(namespace, uri, "?repository=#{repo.folder_name}")
         ctx = context("GET", path, json_headers)
         ctx.route_params = {"id" => uri}
-        ctx.response.output = io
+        ctx.response.output = IO::Memory.new
         Api::Drivers.new(ctx, :index).show
 
         ctx.response.status_code.should eq 200
