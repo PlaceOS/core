c956c365d3898f95a19dc99e5425af7c833f39c2
Stephen von Takach <steve@vontaka.ch>
2021-02-17 12:45:08 +1100

    feat: add logstash support

--- src/core-app.cr
+++ src/core-app.cr
@@ -61,9 +61,11 @@ Signal::TERM.trap &terminate
 
 # Allow signals to change the log level at run-time
 logging = Proc(Signal, Nil).new do |signal|
-  level = signal.usr1? ? Log::Severity::Debug : Log::Severity::Info
-  puts " > Log level changed to #{level}"
-  Log.builder.bind "place_os.core.*", level, PlaceOS::Core::LOG_BACKEND
+  log_level = signal.usr1? ? Log::Severity::Debug : Log::Severity::Info
+  log_backend = PlaceOS::Core.log_backend
+  puts " > Log level changed to #{log_level}"
+  Log.builder.bind "action-controller.*", log_level, log_backend
+  Log.builder.bind "place_os.core.*", log_level, log_backend
   signal.ignore
 end
 
--- src/placeos-core.cr
+++ src/placeos-core.cr
@@ -1,10 +1,36 @@
 require "action-controller/logger"
 require "log_helper"
 require "./placeos-core/*"
+require "./constants"
 
 module PlaceOS::Core
-  Log         = ::Log.for(self)
-  LOG_BACKEND = ActionController.default_backend
+  Log           = ::Log.for(self)
+  LOG_STDOUT    = ActionController.default_backend
+  LOGSTASH_HOST = ENV["LOGSTASH_HOST"]?
+  LOGSTASH_PORT = ENV["LOGSTASH_PORT"]?
+
+  def self.log_backend
+    if !(logstash_host = LOGSTASH_HOST.presence).nil?
+      logstash_port = LOGSTASH_PORT.try(&.to_i?) || abort("LOGSTASH_PORT is either malformed or not present in environment")
+
+      # Logstash UDP Input
+      logstash = UDPSocket.new
+      logstash.connect logstash_host, logstash_port
+      logstash.sync = false
+
+      # debug at the broadcast backend level, however this will be filtered
+      # by the bindings
+      backend = ::Log::BroadcastBackend.new
+      backend.append(LOG_STDOUT, :trace)
+      backend.append(ActionController.default_backend(
+        io: logstash,
+        formatter: ActionController.json_formatter
+      ), :trace)
+      backend
+    else
+      LOG_STDOUT
+    end
+  end
 
   def self.start_managers
     resource_manager = ResourceManager.instance
