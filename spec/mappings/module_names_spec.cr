48905a72dd29007f429476909ccae9871dabcb26
Caspian Baska <caspianbaska@gmail.com>
2021-04-20 16:13:18 +1000

    test: add basic tests for ModuleNames and SettingsUpdates

--- spec/mappings/module_names_spec.cr
+++ spec/mappings/module_names_spec.cr
@@ -1,5 +1,21 @@
 require "../helper"
 
 module PlaceOS::Core::Mappings
-  pending ModuleNames, tags: "resource"
+  describe ModuleNames, tags: "resource" do
+    it "ignores creates" do
+      mod = Model::Module.new
+      ModuleNames.new(ModuleManager.new("https://test.online"))
+        .process_resource(:created, mod)
+        .skipped?
+        .should be_true
+    end
+
+    it "ignores deletes" do
+      mod = Model::Module.new
+      ModuleNames.new(ModuleManager.new("https://test.online"))
+        .process_resource(:deleted, mod)
+        .skipped?
+        .should be_true
+    end
+  end
 end
