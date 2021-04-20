025ca11113635b39eb18bb94e41978d45a09666a
Caspian Baska <caspianbaska@gmail.com>
2021-04-20 16:13:18 +1000

    test: add basic tests for ModuleNames and SettingsUpdates

--- spec/resource_spec.cr
+++
@@ -1,6 +0,0 @@
-require "./helper"
-
-module PlaceOS::Core
-  describe Resource, tags: "resource" do
-  end
-end
--- spec/settings_update_spec.cr
+++ spec/settings_update_spec.cr
@@ -1,5 +1,14 @@
 require "./helper"
 
 module PlaceOS::Core
-  pending SettingsUpdate, tags: "resource"
+  describe SettingsUpdate, tags: "resource" do
+    it "ignores versions" do
+      settings = Model::Settings.new
+      settings.id = "sets-version"
+      settings.settings_id = "sets-parent"
+      settings_update = SettingsUpdate.new(ModuleManager.new("https:://test.online"))
+
+      settings_update.process_resource(:created, settings).skipped?.should be_true
+    end
+  end
 end
