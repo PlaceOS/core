e09e771633eee3f259136d6393c4ab70443a195f
Caspian Baska <caspianbaska@gmail.com>
2021-04-26 14:58:44 +1000

    refactor(config): extract logging to logging.cr

---
+++ src/logging.cr
@@ -0,0 +1,20 @@
+require "placeos-log-backend"
+
+module PlaceOS::Core
+  # Configure logging
+  log_backend = PlaceOS::LogBackend.log_backend
+  log_level = Core.production? ? ::Log::Severity::Info : ::Log::Severity::Debug
+
+  ::Log.setup "*", :warn, log_backend
+
+  namespaces = ["action-controller.*", "place_os.*"]
+  namespaces.each do |namespace|
+    ::Log.builder.bind(namespace, log_level, log_backend)
+  end
+
+  PlaceOS::LogBackend.register_severity_switch_signals(
+    production: Core.production?,
+    namespaces: namespaces,
+    backend: log_backend,
+  )
+end
