33a2a4a5ba858bce44b5b650e05922faecab28d0
Caspian Baska <email@caspian.computer>
2021-07-09 17:33:26 +1000

    refactor(client): tidy up

--- spec/controllers/root_spec.cr
+++ spec/controllers/root_spec.cr
@@ -1,15 +1,18 @@
 require "../helper"
 
 module PlaceOS::Core::Api
-  with_server do
-    it "health checks" do
-      result = curl("GET", "/api/core/v1/")
-      result.status_code.should eq 200
-    end
-    it "should check version" do
-      result = curl("GET", "/api/core/v1/version")
-      result.status_code.should eq 200
-      PlaceOS::Model::Version.from_json(result.body).service.should eq "core"
+  describe Root do
+    with_server do
+      it "health checks" do
+        result = curl("GET", "/api/core/v1/")
+        result.status_code.should eq 200
+      end
+
+      it "should check version" do
+        result = curl("GET", "/api/core/v1/version")
+        result.status_code.should eq 200
+        PlaceOS::Model::Version.from_json(result.body).service.should eq "core"
+      end
     end
   end
 end
