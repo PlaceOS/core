701e97a7aa4c3d23199e7eb35d3e8562a3c7a344
Caspian Baska <email@caspian.computer>
2022-03-07 12:54:18 +1100

    docs: remove `build` todos

--- src/placeos-core/module_manager.cr
+++ src/placeos-core/module_manager.cr
@@ -29,7 +29,6 @@ module PlaceOS::Core
 
     delegate stop, to: clustering
 
-    # TODO: Remove after this is resolved https://github.com/place-technology/roadmap/issues/24
     delegate path_for?, to: local_processes
 
     delegate manage_edge, to: edge_processes
