PlaceOS/core

commit 24117c6517adbd28384c4cb4b30291cba6167a1b

fix: ensure module manager starts in resource manager callback

src/placeos-core.cr
@@ -13,11 +13,8 @@ module PlaceOS::Core
     # Acquire resources on startup
     resource_manager.start do
       # Start managing modules once relevant resources present
-      spawn(same_thread: true) do
-        module_manager.start
-      end
+      spawn(same_thread: true) { module_manager.start }
+      Fiber.yield
     end
-
-    Log.debug { "" }
   end
 end

commit 96d3e1e9fb4bce1e1ea21847e8f5756ccc07f14d

fix: possible race condition in spawn
This spawn was introduced in #59 to solve an issue with an instance being unhealthy during compilation on boot.

However it seems this spawn is surplus to that goal and might introduce race conditions

src/placeos-core.cr
@@ -40,8 +40,7 @@ module PlaceOS::Core
     # Acquire resources on startup
     resource_manager.start do
       # Start managing modules once relevant resources present
-      spawn(same_thread: true) { module_manager.start }
-      Fiber.yield
+      module_manager.start
     end
   end
 