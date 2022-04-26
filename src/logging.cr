811ad3a2b030fa69a11924a0a7ce75473d27bd45
Caspian Baska <email@caspian.computer>
2022-04-26 21:34:22 +1000

    feat(logging): add configuration by LOG_LEVEL env var

--- src/logging.cr
+++ src/logging.cr
@@ -9,12 +9,20 @@ module PlaceOS::Core::Logging
   log_backend = PlaceOS::LogBackend.log_backend
   log_level = Core.production? ? ::Log::Severity::Info : ::Log::Severity::Debug
 
-  ::Log.setup "*", :warn, log_backend
+  builder = ::Log.builder
+  builder.bind "*", log_level, log_backend
 
   namespaces = ["action-controller.*", "place_os.*"]
   namespaces.each do |namespace|
-    ::Log.builder.bind(namespace, log_level, log_backend)
+    builder.bind(namespace, log_level, log_backend)
   end
+
+  ::Log.setup_from_env(
+    default_level: log_level,
+    builder: builder,
+    backend: log_backend,
+    log_level_env: "LOG_LEVEL",
+  )
 
   PlaceOS::LogBackend.register_severity_switch_signals(
     production: Core.production?,
