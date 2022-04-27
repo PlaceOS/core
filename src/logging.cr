4ffd685c5d660336bf97d5c0760b32409d983ae0
Caspian Baska <email@caspian.computer>
2022-04-27 20:09:42 +1000

    feat(logging): configure OpenTelemetry

--- src/logging.cr
+++ src/logging.cr
@@ -1,4 +1,5 @@
 require "placeos-log-backend"
+require "placeos-log-backend/telemetry"
 
 require "./constants"
 
@@ -28,5 +29,10 @@ module PlaceOS::Core::Logging
     production: Core.production?,
     namespaces: namespaces,
     backend: log_backend,
+  )
+
+  PlaceOS::LogBackend.configure_opentelemetry(
+    service_name: APP_NAME,
+    service_version: VERSION,
   )
 end
