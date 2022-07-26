a4ed8fbf0ac9890e64650bcbbe159e84e3615a8e
Caspian Baska <email@caspian.computer>
2022-07-26 10:45:58 +0400

    fix(process_check): fix a race condition (#226)
    * fix(process_check): fix a race condition
    
    * fix: close channel after use
    
    * fix: resolved a potential deadlock
    
    * fix(process_check): pull kill into restart scan
    
    * fix: ignore `State::Dead`
    
    * fix: timeout is a bad shard, sorry
    
    * Revert "fix: ignore `State::Dead`"
    
    This reverts commit 0eb90c9bbc5c1d7e8dad06a5b1ee6b6d6c5f5969.
    
    * feat: add state to retart log

--- src/placeos-core/process_check.cr
+++ src/placeos-core/process_check.cr
@@ -19,61 +19,110 @@ module PlaceOS::Core
       @process_check_task.try &.cancel
     end
 
+    enum State
+      # Process is running
+      Running
+      # Process is not responding
+      Unresponsive
+      # Process is dead
+      Dead
+    end
+
     # TODO:
-    # Add `with_module_managers` to ProcessManager interface
-    # and thus provide process check support for edge nodes.
+    # This could also be performed independently in the `Edge::Client`.
     #
-    # NOTE: This could also be performed independently in the `Edge::Client` instead of here.
+    # - Add `with_module_managers` to ProcessManager interface
+    #
+    # - Create a `load_module` method that works with `module_id`s, using the dead pm's module_id => payload hash.
+    #   See the note in the method below on where to hook this in.
+    #
     protected def process_check : Nil
       Log.debug { "checking for dead driver processes" }
 
-      # NOTE: Add `edge_processes` here once it supports process check
-      {local_processes}.each &.with_module_managers do |module_manager_map|
+      # NOTE:
+      # Generally, having an unbounded channel is a bad idea, as it can cause deadlocks.
+      # The ideal would be having a channel the size of the number of managers we're inspecting.
+      checks = Channel({State, {Driver::Protocol::Management, Array(String)}}).new
+
+      total_protocol_managers = local_processes.with_module_managers do |module_manager_map|
         # Group module keys by protcol manager
         grouped_managers = module_manager_map.each_with_object({} of Driver::Protocol::Management => Array(String)) do |(module_id, protocol_manager), grouped|
           (grouped[protocol_manager] ||= [] of String) << module_id
         end
 
-        # Check if any processes are dead
+        # Asynchronously check if any processes are timing out on comms, and if so, restart them
         grouped_managers.each do |protocol_manager, module_ids|
           # Asynchronously check if any processes are timing out on comms, and if so, restart them
           spawn(same_thread: true) do
-            process_alive = begin
+            state = begin
               # If there's an empty response, the modules that were meant to be running are not.
               # This is taken as a sign that the process is dead.
               # Alternatively, if the response times out, the process is dead.
+              info = nil
               timeout(PROCESS_COMMS_TIMEOUT) do
-                !protocol_manager.info.empty?
+                info = protocol_manager.info
               end
-            rescue error
+
+              if info.nil? || info.as(Array(String)).empty?
+                State::Dead
+              else
+                State::Running
+              end
+            rescue error : Timeout::Error
               Log.warn(exception: error) { "unresponsive process manager for #{module_ids.join(", ")}" }
-              false
+              State::Unresponsive
             end
 
-            unless process_alive
-              Log.warn { {message: "restarting unresponsive driver", driver_path: protocol_manager.@driver_path} }
-              # Ensure the process is dead.
-              begin
-                Process.signal(Signal::KILL, protocol_manager.pid)
-              rescue
-              end
+            checks.send({state, {protocol_manager, module_ids}})
+          end
+        end
 
-              # Remove the dead manager from the map
-              module_manager_map.reject(module_ids)
+        grouped_managers.size
+      end
 
-              # Restart all the modules previously assigned to the dead manager
-              #
-              # TODO:
-              # Make this independent of a database query by using the dead manager's stored module_ids and payloads.
-              # This will allow this module to be included in `PlaceOS::Edge::Client`.
-              # To do so, one will need to create the module manager (currently done by the `load_module` below (which is in `PlaceOS::Core::ModuleManager`))
-              Model::Module.find_all(module_ids).each do |mod|
-                load_module(mod)
-              end
-            end
+      local_processes.with_module_managers do |module_manager_map|
+        # Synchronously handle restarting unresponsive/dead drivers
+        total_protocol_managers.times do
+          state, driver_state = checks.receive
+          protocol_manager, module_ids = driver_state
+
+          next if state.running?
+
+          # Ensure the process is killed
+          if state.unresponsive?
+            Process.signal(Signal::KILL, protocol_manager.pid) rescue nil
+          end
+
+          # Kill the process manager's IO, unblocking any fibers waiting on a response
+          protocol_manager.@io.try(&.close) rescue nil
+
+          Log.warn { {message: "restarting unresponsive driver", state: state.to_s, driver_path: protocol_manager.@driver_path} }
+
+          # Determine if any new modules have been loaded onto the driver that needs to be restarted
+          fresh_module_ids = module_manager_map.compact_map do |module_id, pm|
+            module_id if pm == protocol_manager
+          end
+
+          # Union of old and new module_ids
+          module_ids |= fresh_module_ids
+
+          # Remove the dead manager from the map
+          module_manager_map.reject(module_ids)
+
+          # Restart all the modules previously assigned to the dead manager
+          #
+          # NOTE:
+          # Make this independent of a database query by using the dead manager's stored module_ids and payloads.
+          # This will allow this module to be included in `PlaceOS::Edge::Client`.
+          # To do so, one will need to create the module manager (currently done by the `load_module` below (which is in `PlaceOS::Core::ModuleManager`))
+          Model::Module.find_all(module_ids).each do |mod|
+            Log.debug { "reloading #{mod.id} after restarting unresponsive driver" }
+            load_module(mod)
           end
         end
       end
+
+      checks.close
     end
   end
 end
